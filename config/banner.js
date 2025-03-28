d399c74b96cabdfe0996d7a1b050c44bb58efd58
lLana <pp71802428@gmail.com>
2025-03-28 14:14:37 +0700

    first commit

--- config/banner.js
+++ config/banner.js
@@ -10,7 +10,7 @@ function displayBanner() {
   console.log(chalk.green(banner));
   console.log(chalk.yellow('========================================='));
   console.log(chalk.magenta('Github   : https://github.com/0x-Disciple'));
-  console.log(chalk.magenta('Telegram : https://t.me/CryptoKizs'))
+  console.log(chalk.magenta('Telegram : https://t.me/CryptoKidzs'))
   console.log(chalk.yellow('========================================='))
 }
 displayBanner();
